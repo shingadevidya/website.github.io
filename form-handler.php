5c36e667613cb4dadd92fde6fc9ba833763a42e5
shingadevidya <shingadevidya912@gmail.com>
2024-10-24 17:16:38 +0530

    main

--- form-handler.php
+++ form-handler.php
@@ -4,7 +4,7 @@ $visitor_email = $_POST['email'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
-$email_form = 'info@solution.com';
+$email_form = 'info@website.github.io';
 
 $email_subject = 'New Form Submission';
 
